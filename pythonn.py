0afe280758f9e4a99bcdb2323598aff462f01c83
shanel-11 <shaneldiko123@gmail.com>
2026-02-23 16:57:32 +0100

    Create pythonn.py

---
+++ pythonn.py
@@ -0,0 +1,60 @@
+my_name= "shanel"
+my_age= "18"
+my_favouritecolor= "black "
+print(my_name+","+ my_age+","+my_favouritecolor)
+
+integer_1= 3
+integer_2= 6
+integer_3= 1
+print(integer_1+integer_2+integer_3)
+
+school_name="protagonistet "
+grade="12 "
+fav_sub="physics"
+print(school_name+grade+fav_sub)
+
+
+kmtom= "10km=10000m "
+htos="5h=18000s "
+print(kmtom+htos)
+
+boxes_of_cereal=5
+boxesp= 10
+total1= 5*10
+cans=23
+cansp=10
+total2=23*10
+print(total1+total2
+)
+
+
+
+input("hello")
+print(input)
+
+
+input("my name is sara i am 26 and i live in spain")
+print(input)
+
+#integer1="nr1:"
+#integer2="nr2:"
+stored_input1=int(input("nr1:"))
+stored_input2=int(input("nr2:"))
+print("the sum is" , stored_input1+stored_input2)
+
+
+min=int(input("minutes:"))
+sec= min*60
+h= min/60
+print("sec:", sec)
+print("h", h)
+
+
+a=int(input("a:"))
+b=int(input("b:"))
+area=a*b
+p=a+b
+print("area:",area , "p:", p )
+
+
+
